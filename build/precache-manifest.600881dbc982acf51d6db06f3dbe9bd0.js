c364545a0dcc0d574b50a6cb1577c293de02a8d9
Pranav Harsh <pranavharsh@Pranavs-MacBook-Air.local>
2020-08-10 10:21:17 -0700

    adding new project

---
+++ build/precache-manifest.600881dbc982acf51d6db06f3dbe9bd0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "acda0446b60173c205265f820eaddf63",
+    "url": "/MONSTERS-FRIENDS/index.html"
+  },
+  {
+    "revision": "6ef80f1f711c5ce35520",
+    "url": "/MONSTERS-FRIENDS/static/css/main.26850d11.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/MONSTERS-FRIENDS/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/MONSTERS-FRIENDS/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ef80f1f711c5ce35520",
+    "url": "/MONSTERS-FRIENDS/static/js/main.17c5c261.chunk.js"
+  },
+  {
+    "revision": "fe03d0cf7f3dfd58e8a0",
+    "url": "/MONSTERS-FRIENDS/static/js/runtime-main.7e060a3b.js"
+  }
+]);
